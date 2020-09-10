ba8aa190d2a6de5c6fe4923be0d12acf3a43a490
github username <github email address>
2020-09-10 11:06:00 -0400

    stop selling tickets

--- src/index.js
+++ src/index.js
@@ -25,37 +25,51 @@ document.addEventListener('DOMContentLoaded', (event) => {
     }
 
     function clickListener(){
+     
         const button = document.querySelector("#button-container")
         const targetButton = document.querySelector("#button-container").children[0]
-     button.addEventListener('click', (event) => {
+     
+        button.addEventListener('click', (event) => {
+            
+            event.preventDefault();
+
          if (event.target === targetButton) {
+            fetch(url)
+            .then(response =>response.json())
+            .then(movie => {
+
            let ticketSold = 0
             const ticketNum = parseInt(movieTickets.innerText)  
             movieTickets.innerText = ticketNum - 1
-            ticketSold = ticketSold + 1
-
+           ticketSold = movie.tickets_sold + 1
+            
+           if (ticketNum < 1) {
+                console.log("Sorry Sold out !")
+            }
+            if (ticketNum > 0 ) {
             updateTickets(ticketSold)
-           
+          }
+        })
        }
      })
     }
 
     function updateTickets(ticketSold) {
-        options = {
-            methods: "PATCH",
+        let options = {
+            method: "PATCH",
             headers: {
                 "Content-Type": "application/json",
-                "Aceept" : "application/json"
+                "Accept" : "application/json"
             },
             body: JSON.stringify({
                 tickets_sold: ticketSold
             })
         }
-        
-        fetch(url + 1, options)
+        // "http://localhost:3000/films/[1]"
+        fetch(url, options)
         .then(response => response.json())
         .then(ticketData => {
-            console.log(ticketData)
+            movieTickets.innerText = ticketData.tickets_sold
         })
     }
 
