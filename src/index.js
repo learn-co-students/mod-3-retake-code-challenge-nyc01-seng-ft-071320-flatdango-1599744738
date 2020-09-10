aa07ecb8321a92ef6a7ae186e6f28da83e8b3ade
iuriseara <iuriseara@gmail.com>
2020-09-10 10:00:53 -0400

    FirstCommit

--- src/index.js
+++ src/index.js
@@ -1,1 +1,23 @@
 const url = "http://localhost:3000/films"
+
+function getMovies() {
+    fetch(url)
+    .then(response => response.json())
+    .then(movies => movies.forEach(movie => renderMovie(movie)))   
+}
+
+function renderMovie(movie) {
+    const poster = document.querySelector('#poster')
+    const filmItem = document.querySelector('.film item')
+    const listContainer = document.querySelector('.list-container')
+    const divList = document.querySelector('.ui divided list')
+    const card = document.querySelector('.card')
+    const title = document.querySelector('#title')
+    const runtime = document.querySelector('#runtime')
+    const showtime = document.querySelector('#showtime')
+
+    
+    debugger
+}
+
+getMovies()
