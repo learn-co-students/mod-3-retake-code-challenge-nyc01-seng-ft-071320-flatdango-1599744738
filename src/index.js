5e7cf881e5446cbc20487c764472428180bbaccf
“Mineliegoma” <garlandegoma@gmail.com>
2020-09-10 10:22:20 -0400

    deliverable_one

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-const url = "http://localhost:3000/films"
+
+document.addEventListener('DOMContentloaded', e => {
+ const url = "http://localhost:3000/films/1"
+ function fetchMovies() {
+  fetch(url)
+   .then(res => res.json())
+   .then(movie => {
+    renderMovie(movie)
+
+   })
+  const renderMovie = movie => {
+   //console.log(movie)
+   const title = movie.title
+   const poster = movie.poster
+   const runtime = movie.runtime
+   const showtime = movie.showtime
+   const available_ticket = movie.
+
+
+
+
+  }
+
+
+
+
+
+
+
+ }
+ fetchMovies()
+})
