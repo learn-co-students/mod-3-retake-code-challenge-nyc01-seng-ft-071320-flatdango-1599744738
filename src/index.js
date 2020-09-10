54c0a6ceea3c247747c9a6ecf9e2bafb082fb61e
Lazarus22 <matthew.peters.nyc@gmail.com>
2020-09-10 10:01:02 -0400

    deliverables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-const url = "http://localhost:3000/films"
+document.addEventListener("DOMcontentLoaded", function(e){
+
+    const url = "http://localhost:3000/films"
+
+    const fetchMovie = () => {
+        fetch(url)
+            .then(resp => resp.json())
+            .then(renderMovie)
+    }
+
+    const renderMovie = (movie) => {
+        const movieDiv = document.querySelector("#showing")
+        const movieCard = document.createElement('div')
+        movieCard.className = 'card'
+        movieCard.innerHTML = `
+            <div id="title" class="title">${movie.title}</div>
+            <div id="runtime" class="meta">${movie.runtime}</div>
+            <div class="content">
+            <div class="description">
+            <div id="film-info">${movie.description}</div>
+            <span id="showtime" class="ui label">${movie.showtime}</span>
+            <span id="ticket-num">${movie.tickets_sold}</span> remaining tickets
+        `
+        movieDiv.append(movieCard)
+    }
+    fetchMovie()
+
+
+
+
+
+})//dom loaded
