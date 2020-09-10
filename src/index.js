c03f1ed6f76e235eb67782e7f0f75815660b9269
kevin_xie <xie.kevin.j@gmail.com>
2020-09-10 11:22:24 -0400

    wutamidoing

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ document.addEventListener("DOMContentLoaded", () => {
   clickHandler();
 });
 
+let ticketsSold = 0;
 // const url = "http://localhost:3000/films";
 const ce = (tag) => document.createElement(tag);
 const qs = (selector) => document.querySelector(selector);
+let filmDiv = qs(".card");
 
 const fetchFilms = () => {
   fetch("http://localhost:3000/films/1")
@@ -14,39 +16,55 @@ const fetchFilms = () => {
 };
 
 const renderFilm = (film) => {
-  let filmDiv = qs(".card");
-  filmDiv.innerHTML = `
-  <div id="title" class="title">[${film.title}]</div>
-  <div id="runtime" class="meta">[${film.runtime}] minutes</div>
-  <div class="content">
-      <div class="description">
-          <div id="film-info">[${film.description}]</div>
-          <span id="showtime" class="ui label">[${film.showtime}]</span>
-          <span id="ticket-num">[X]</span> remaining tickets
-      </div>
-      <div class="extra content">
-      <div class="ui orange button">Buy Ticket</div>
-    </div>
-  </div>
-  </div>`;
+  ticketsSold = film.tickets_sold;
+  const remainingTickets = film.capacity - film.tickets_sold;
+  let buyTicketButton = "";
+  if (remainingTickets > 0) {
+    buyTicketButton = `<div class="ui orange button">Buy Ticket</div>`;
+  }
 
-  //   let filmPoster = qs("#poster");
-  //   incomingPoster = document.createElement("IMG");
-  //   incomingPoster = ${film.poster}
-  //   filmPoster.append(incomingPoster);
-  //poster, title, runtime, showtime, and available tickets
+  filmDiv.innerHTML = `
+      <div id="title" class="title">[${film.title}]</div>
+      <div id="runtime" class="meta">[${film.runtime}] minutes</div>
+      <div class="content">
+          <div class="description">
+              <div id="film-info">[${film.description}]</div>
+              <span id="showtime" class="ui label">${film.showtime}</span>
+              <span id="ticket-num">${remainingTickets}</span> remaining tickets
+          </div>
+          <div class="extra content">
+            ${buyTicketButton}
+          </div>
+      </div>`;
+  let poster = qs(".four.wide.column");
+  let posterPic = ce("div");
+  posterPic.innerHTML = `<img id="poster" src="${film.poster}"/>`;
+  poster.append(posterPic);
 };
-// const renderFilm = (film) => {
-//   const filmDiv = qs(".filmItem");
-// };
 
 const clickHandler = () => {
   document.addEventListener("click", (e) => {
-    // look for clicks on span
-    const orangeButton = qs("div.ui-orange-button");
-    console.log(e.target);
-    if (e.target.matches(".ui orange button")) {
-      console.log(e.target);
+    e.preventDefault();
+    if (e.target.matches(".ui.orange.button")) {
+      const filmData = {
+        tickets_sold: ticketsSold + 1,
+      };
+
+      renderClick(filmData);
     }
   });
+};
+
+const renderClick = (filmData) => {
+  let configObj = {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(filmData),
+  };
+  fetch("http://localhost:3000/films/1", configObj)
+    .then((response) => response.json())
+    .then((film) => renderFilm(film));
 };
