eefabf9708f6a73391bf41adb32f4ba9b0f842c7
iuriseara <iuriseara@gmail.com>
2020-09-10 10:45:42 -0400

    Finished 1st Deliverable

--- src/index.js
+++ src/index.js
@@ -15,10 +15,21 @@ function renderMovie(movie) {
     const card = document.querySelector('.card')
     const title = document.querySelector('#title')
     const runtime = document.querySelector('#runtime')
+    const description = document.querySelector('#film-info')
     const showtime = document.querySelector('#showtime')
+    const ticketNum = document.querySelector('#ticket-num')
+
+    let theaterCapacity = movie.capacity
+    let tickets_sold = movie.tickets_sold
+    let remainingTickets = theaterCapacity - tickets_sold
+
+    ticketNum.innerHTML = remainingTickets
 
     title.dataset.title = movie.title
     title.innerHTML = movie.title
+
+    description.dataset.description = movie.description
+    description.innerHTML = movie.description
 
     runtime.dataset.runtime = movie.runtime
     runtime.innerHTML = `${movie.runtime} minutes` 
@@ -27,15 +38,16 @@ function renderMovie(movie) {
     showtime.innerHTML = movie.showtime
 
     poster.dataset.poster = movie.poster
-    poster.innerHTML = `
-    <img id="poster" src="assets/placeholderImage.png" data-poster= ${movie.poster}>
-    `
-    
-
+    poster.src = movie.poster
 
     posterContainer.appendChild(poster)
     title.append(card)
     // debugger
 }
+
+const buyButton = () => {
+    
+}
+
 
 getMovies()
