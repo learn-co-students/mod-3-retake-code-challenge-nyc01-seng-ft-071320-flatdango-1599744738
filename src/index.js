55053e060514821a1dfddc2a86291a1224812b0f
DBenson1017 <devinbenson@gmail.com>
2020-09-10 11:17:47 -0400

    core deliverables done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function(e){
-    loadFrontMovie(1)
+    loadFrontMovie()
 }) 
 
 //### variables #########
@@ -13,13 +13,13 @@ document.addEventListener('click', function(e){
     e.preventDefault()
     console.log(e.target)
      if(e.target.matches('#buy-ticket-btn')){     console.log('buy ticket clicked')
-     buyTicket(e.target)
+     lowerTicketDOM(e.target)
     }
 })// end of click listener 
 
 //####### load data for movie one
-function loadFrontMovie(id){
-    fetch(url + id)
+function loadFrontMovie(){
+    fetch("http://localhost:3000/films/1")
     .then(function(response){
         return response.json()
     })
@@ -40,16 +40,53 @@ function renderFrontMovie(data){
     document.querySelector('#ticket-num').innerText=tix
     document.querySelector('#film-info').innerText=data.description
     document.querySelector('.card').dataset.id=data.id
+    document.querySelector('.card').dataset.remaining=(parseInt(data.capacity, 10) - parseInt(data.tickets_sold, 10))
+    document.querySelector('.card').dataset.sold=data.tickets_sold
 }
 
 
 //#### PATCH request to buy ticket
-function buyTicket(btn){
-    // change DOM 
-    doc
-   let card = btn.closest('.card')
-   let id = card.dataset.id 
-   
-   let data = {tickets_sold: }
+function lowerTicketDOM(btn){
+    let card = btn.closest('.card')
+    let availableTickets = card.dataset.remaining
+    console.log(availableTickets)
+    if(availableTickets >0){
+    document.querySelector('#ticket-num').innerText=(
+        parseInt(availableTickets, 10) - 1)
+    lowerTicketDb(btn)
+    } else {window.alert(`I'm sorry, there are no more tickets!`)}
+        
+    }// 
+    
 
-}// 
+    function lowerTicketDb(btn){
+        let card = btn.closest('.card')
+        let id = card.dataset.id 
+        console.log(id)
+        let sold = parseInt(card.dataset.sold, 10)
+        let update = (sold + 1)
+        console.log(update)
+
+        let data = {tickets_sold: update}
+        console.log(data)
+
+        let options = {
+            method: 'PATCH',
+            headers: {
+                'Content-Type': 'application/json',
+                'accept': 'application/json'
+            },
+            body: JSON.stringify({tickets_sold: update})
+        }
+
+        fetch(url+id, options)
+            .then(function(response){
+                return response.json
+            })
+            .then(function(response){
+                console.log(response)
+                loadFrontMovie()
+            })
+
+       
+}
